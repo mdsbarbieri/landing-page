168cb9a508f4249b3aebf7c21eb5c0d8bebbbc7d
Matheus Barbieri <mdsbarbieri@gmail.com>
2022-03-14 17:29:33 -0300

    chore: Add font family configuration

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,5 @@
-const  Home = () => {
-  return (
-      <h1 className="text-3xl font-bold text-indigo-600">
-        Hello world!
-      </h1>
-  )
-}
+const Home = () => {
+  return <h1 className="text-3xl font-bold text-indigo-600">Hello world!</h1>;
+};
 
-export default Home
+export default Home;
