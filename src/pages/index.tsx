9e914dfa5dd106fa216576c9c15b0133c1c59158
mdsbarbieri <mdsbarbieri@gmail.com>
2022-06-20 19:03:58 -0300

    Update Landing Page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,10 @@
 import MainHero from '../components/hero/MainHero';
-import { MetaTags } from '../global/MetaTags';
-import HeroTeam from '../components/hero/HeroTeam';
-import Offers from '../components/hero/Offers';
-import Jobs from '../components/jobs/Jobs';
 
 const Home = () => {
   return (
     <>
-      <MetaTags title="Elo7 - Trabalhe Conosco" description="Trabalhe no Elo7" />
+      <MetaTags title="Mdsbarbieri" description="Mdsbarbieri" />
       <MainHero />
-      <HeroTeam />
-      <Offers />
-      <Jobs />
     </>
   );
 };
